8b941df498f2fc727db7d452e9a590be4cf3b273
venumashetty <venu.mashetty@daimlertruck.com>
2026-02-11 17:55:40 -0800

    commit

--- app/day1.py
+++ app/day1.py
@@ -2,6 +2,7 @@
 # Connect to Snowflake
 
 import streamlit as st
+import os
 
 st.title(":material/vpn_key: Day 1: Connect to Snowflake")
 
@@ -10,10 +11,28 @@ try:
     # Works in Streamlit in Snowflake
     from snowflake.snowpark.context import get_active_session
     session = get_active_session()
-except:
-    # Works locally and on Streamlit Community Cloud
+
+except Exception:
+    # Works locally
     from snowflake.snowpark import Session
-    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
+
+    conn = st.secrets["connections"]["snowflake"]
+
+    pk_path = conn["private_key_path"]
+
+    if not os.path.exists(pk_path):
+        raise FileNotFoundError(f"Private key not found at {pk_path}")
+
+    with open(pk_path, "rb") as f:
+        private_key_bytes = f.read()
+
+    configs = dict(conn)
+    configs["private_key"] = private_key_bytes
+
+    # Remove path so Snowpark doesn't get confused
+    configs.pop("private_key_path", None)
+
+    session = Session.builder.configs(configs).create()
 
 # Query Snowflake version
 version = session.sql("SELECT CURRENT_VERSION()").collect()[0][0]
